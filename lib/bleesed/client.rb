9636e5ac33e54854d62641066603b5fb2621165b
Shaun Garwood <shaun.garwood@gmail.com>
2024-04-04 14:42:29 -0500

    buncha changes

--- lib/bleesed/client.rb
+++ lib/bleesed/client.rb
@@ -4,15 +4,20 @@ URL = "https://app.blesseveryhome.com/"
 
 module Bleesed
   class Client
+    attr_reader :email, :proxy, :token, :role_id, :other_roles
     # GOABOUT: pass options hash to faraday
     def initialize(email: nil, password: nil, proxy: nil)
       @email = email
       @password = password
       @proxy = proxy
       @token = nil
+      @role_id = nil
+      @other_roles
     end
 
     def login!
+      get_token
+
       response = connection.post("login/index.php",
         {
           forgodseternalpurpose: token,
@@ -22,7 +27,25 @@ module Bleesed
           password: @password
         }
       )
-      response.status
+
+      redirect = response.headers["location"]
+
+      if redirect && redirect.include?("/light/progress/?pagetype=light&pagerole=")
+        @role_id = redirect.split("pagerole=").last
+        get_other_roles(redirect)
+
+        @role_id
+      elsif response.body.include?('Incorrect credentials given!')
+        raise UnauthorizedError, "Login failed"
+      else
+        raise Error, "Login failed"
+      end
+    end
+
+    def logout!
+      response = connection.get("login/logout.php")
+      response.status == 302 &&
+        response.headers["location"] == URL + "login/index.php"
     end
 
     private
@@ -36,12 +59,25 @@ module Bleesed
       end
     end
 
-    def token
+    def get_token
       res = connection.get("login/index.php")
       doc = Nokogiri::HTML(res.body)
-      doc.css('input[name="forgodseternalpurpose"]').first["value"]
+      @token = doc.css('input[name="forgodseternalpurpose"]').first["value"]
+    end
+
+    def get_other_roles(link)
+      response = connection.get(link)
+      doc = Nokogiri::HTML(response.body)
+      roles = doc.css('a.statusBarMyAcountsDropdown-row')
+      @other_roles = roles.map do |role|
+        { name: role.text.strip, link: role['href'] }
+      end
+      @other_roles.delete_if { |role| role[:link] == '/person' }
+      @other_roles
     end
   end
 
   class Error < StandardError; end
+
+  class UnauthorizedError < Error; end
 end
--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -8,9 +8,52 @@ RSpec.describe Bleesed::Client do
   end
 
   describe "#login!" do
-    it "returns a redirect" do
+    it "returns and saves the role id" do
+      role_id_regex = /^\d{6,}$/
+
       VCR.use_cassette("login") do
-        expect(client.login!).to eq(302)
+        expect(client.login!).to match(role_id_regex)
+        expect(client.role_id).to match(role_id_regex)
+      end
+    end
+
+    context "with invalid login" do
+      it "returns false" do
+        VCR.use_cassette("login_invalid") do
+          expect(client.login!).to eq(false)
+        end
+      end
+    end
+
+    it "saves the token" do
+      VCR.use_cassette("login") do
+        client.login!
+        expect(client.token).not_to be_empty
+        expect(client.token.size).to eq(32)
+      end
+    end
+
+    it "saves the other roles" do
+      VCR.use_cassette("login") do
+        client.login!
+        expect(client.other_roles).not_to be_empty
+        expect(client.other_roles.first.keys).to match_array(%i[name link])
+      end
+    end
+
+    context "with an invalid login" do
+      it "raises an error" do
+        VCR.use_cassette("login_invalid") do
+          expect { client.login! }.to raise_error(Bleesed::UnauthorizedError)
+        end
+      end
+    end
+  end
+
+  describe "#logout!" do
+    it "returns something" do
+      VCR.use_cassette("logout") do
+        expect(client.logout!).to eq(true)
       end
     end
   end
